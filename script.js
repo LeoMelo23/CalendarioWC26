e60a5514773b8db4e4445e04421049ce6571931a
LeoMelo23 <leomelo2332@gmail.com>
2026-05-31 22:56:51 -0300

    adição de todos os jogos de todas as seleções

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ function abrirModal(pais) {
         <div class="jogo-card">
             <span class="jogo-data">${jogo.data} às ${jogo.hora}</span>
             <div class="confronto">
-               <span style="color: ${coresSelecoes[jogo.casa] || 'white'}">${jogo.casa}</span>
+               <span class="selecao" style="color: ${coresSelecoes[jogo.casa] || 'white'}">${jogo.casa}</span>
                 <span class="placar">${jogo.placar}</span>
-                <span style="color: ${coresSelecoes[jogo.fora] || 'white'}">${jogo.fora}</span>
+                <span class="selecao" style="color: ${coresSelecoes[jogo.fora] || 'white'}">${jogo.fora}</span>
             </div>
             <span class="jogo-local">📍 ${jogo.local}</span>
             <span class="status ${jogo.status}">${jogo.status.toUpperCase()}</span>
@@ -67,33 +67,6 @@ document.querySelectorAll('.bandeira').forEach(bandeira => {
     })
 })
 const jogos = [
-    {
-        data: '13/06/2026',
-        hora: '19:00',
-        casa: 'Brasil',
-        fora: 'Marrocos',
-        placar: '- : -',
-        local: 'MetLife Stadium, Los Angeles',
-        status: 'agendado'
-    },
-    {
-        data: '19/06/2026',
-        hora: '21:30',
-        casa: 'Brasil',
-        fora: 'Haiti',
-        placar: '- : -',
-        local: 'Lincoln Financial Field, Filadélfia',
-        status: 'agendado'
-    },
-    {
-        data: '24/06/2026',
-        hora: '19:00',
-        casa: 'Escócia',
-        fora: 'Brasil',
-        placar: '- : -',
-        local: 'Hard Rock Stadium, Miami Gardens',
-        status: 'agendado'
-    },
      {
         data: '11/06/2026',
         hora: '16:00',
@@ -139,6 +112,15 @@ const jogos = [
         local: 'Levis Stadium, Santa Clara',
         status: 'agendado'
     },
+      {
+        data: '13/06/2026',
+        hora: '19:00',
+        casa: 'Brasil',
+        fora: 'Marrocos',
+        placar: '- : -',
+        local: 'MetLife Stadium, Los Angeles',
+        status: 'agendado'
+    },
     {
         data: '13/06/2026',
         hora: '22:00',
@@ -148,6 +130,7 @@ const jogos = [
         local: 'Gillete Stadium, Foxborough',
         status: 'agendado'
     },
+
      {
         data: '14/06/2026',
         hora: '01:00',
@@ -213,7 +196,7 @@ const jogos = [
     },
      {
         data: '15/06/2026',
-        hora: '13:00',
+        hora: '19:00',
         casa: 'Arábia Saudita',
         fora: 'Uruguai',
         placar: '- : -',
@@ -283,11 +266,457 @@ const jogos = [
         local: 'AT&T Stadium, Arlington',
         status: 'agendado'
     },
+     {
+        data: '17/06/2026',
+        hora: '20:00',
+        casa: 'Gana',
+        fora: 'Panamá',
+        placar: '- : -',
+        local: 'BMO Field, Toronto',
+        status: 'agendado'
+    },
+     {
+        data: '17/06/2026',
+        hora: '23:00',
+        casa: 'Uzbequistão',
+        fora: 'Colômbia',
+        placar: '- : -',
+        local: 'Estádio Azteca, Cidade do México',
+        status: 'agendado'
+    },
+     {
+        data: '18/06/2026',
+        hora: '13:00',
+        casa: 'Republica Tcheca',
+        fora: 'África do Sul',
+        placar: '- : -',
+        local: 'Mercedes-Benz Stadium, Atlanta',
+        status: 'agendado'
+    },
+     {
+        data: '18/06/2026',
+        hora: '16:00',
+        casa: 'Suíça',
+        fora: 'Bósnia e Herzegovina',
+        placar: '- : -',
+        local: 'Sofi Estadium, Inglewood',
+        status: 'agendado'
+    },
+     {
+        data: '18/06/2026',
+        hora: '19:00',
+        casa: 'Canadá',
+        fora: 'Qatar',
+        placar: '- : -',
+        local: 'BC Place, Vancouver',
+        status: 'agendado'
+    },
+     {
+        data: '18/06/2026',
+        hora: '22:00',
+        casa: 'México',
+        fora: 'Coréia do Sul',
+        placar: '- : -',
+        local: 'Estádio Akron, Zapopan',
+        status: 'agendado'
+    },
+     {
+        data: '19/06/2026',
+        hora: '16:00',
+        casa: 'EUA',
+        fora: 'Austrália',
+        placar: '- : -',
+        local: 'Lumen Field, Seattle',
+        status: 'agendado'
+    },
+      {
+        data: '19/06/2026',
+        hora: '19:00',
+        casa: 'Escócia',
+        fora: 'Marrocos',
+        placar: '- : -',
+        local: 'Gillete Stadium, FoxBorough',
+        status: 'agendado'
+    },
+     {
+        data: '19/06/2026',
+        hora: '21:30',
+        casa: 'Brasil',
+        fora: 'Haiti',
+        placar: '- : -',
+        local: 'Lincoln Financial Field, Filadélfia',
+        status: 'agendado'
+    },
+      {
+        data: '20/06/2026',
+        hora: '00:00',
+        casa: 'Turquia',
+        fora: 'Paraguai',
+        placar: '- : -',
+        local: 'Levi Stadium, Santa Clara',
+        status: 'agendado'
+    },
+      {
+        data: '20/06/2026',
+        hora: '14:00',
+        casa: 'Holanda',
+        fora: 'Suécia',
+        placar: '- : -',
+        local: 'NRG Stadium, Houston',
+        status: 'agendado'
+    },
+     {
+        data: '20/06/2026',
+        hora: '17:00',
+        casa: 'Alemanha',
+        fora: 'Costa do Marfim',
+        placar: '- : -',
+        local: 'BMO Field, Toronto',
+        status: 'agendado'
+    },
+     {
+        data: '20/06/2026',
+        hora: '21:00',
+        casa: 'Equador',
+        fora: 'Curaçao',
+        placar: '- : -',
+        local: 'Arrowhead Stadium, Kansas City',
+        status: 'agendado'
+    },
+     {
+        data: '21/06/2026',
+        hora: '01:00',
+        casa: 'Tunísia',
+        fora: 'Japão',
+        placar: '- : -',
+        local: 'Estádio BBVA, Guadaloupe',
+        status: 'agendado'
+    },
+     {
+        data: '21/06/2026',
+        hora: '13:00',
+        casa: 'Espanha',
+        fora: 'Arábia Saudita',
+        placar: '- : -',
+        local: 'Mercedes-Benz Stadium, Atlanta',
+        status: 'agendado'
+    },
+     {
+        data: '21/06/2026',
+        hora: '16:00',
+        casa: 'Bélgica',
+        fora: 'Irã',
+        placar: '- : -',
+        local: 'Sofi Stadium, Inglewood',
+        status: 'agendado'
+    },
+     {
+        data: '21/06/2026',
+        hora: '19:00',
+        casa: 'Uruguai',
+        fora: 'Cabo Verde',
+        placar: '- : -',
+        local: 'Hard Rock Stadium, Miami Gardens',
+        status: 'agendado'
+    },
+     {
+        data: '21/06/2026',
+        hora: '22:00',
+        casa: 'Nova Zelândia',
+        fora: 'Egito',
+        placar: '- : -',
+        local: 'BC Place, Vancouver',
+        status: 'agendado'
+    },
+     {
+        data: '22/06/2026',
+        hora: '14:00',
+        casa: 'Argentina',
+        fora: 'Áustria',
+        placar: '- : -',
+        local: 'AT&T Stadium, Arlington',
+        status: 'agendado'
+    },
+     {
+        data: '22/06/2026',
+        hora: '18:00',
+        casa: 'França',
+        fora: 'Iraque',
+        placar: '- : -',
+        local: 'Lincoln Financial Field, Filadélfia',
+        status: 'agendado'
+    },
+     {
+        data: '22/06/2026',
+        hora: '21:00',
+        casa: 'Noruega',
+        fora: 'Senegal',
+        placar: '- : -',
+        local: 'MetLife, East Rutheford',
+        status: 'agendado'
+    },
+     {
+        data: '23/06/2026',
+        hora: '00:00',
+        casa: 'Jordânia',
+        fora: 'Argélia',
+        placar: '- : -',
+        local: 'Levis Stadium, Santa Clara',
+        status: 'agendado'
+    },
+     {
+        data: '23/06/2026',
+        hora: '14:00',
+        casa: 'Portugal',
+        fora: 'Uzbequistão',
+        placar: '- : -',
+        local: 'NRG Stadium, Houston',
+        status: 'agendado'
+    },
+     {
+        data: '23/06/2026',
+        hora: '17:00',
+        casa: 'Inglaterra',
+        fora: 'Gana',
+        placar: '- : -',
+        local: 'Gillete Stadium, Foxborough',
+        status: 'agendado'
+    },
+     {
+        data: '23/06/2026',
+        hora: '20:00',
+        casa: 'Panamá',
+        fora: 'Croácia',
+        placar: '- : -',
+        local: 'BMO Field, Toronto',
+        status: 'agendado'
+    },
+     {
+        data: '23/06/2026',
+        hora: '23:00',
+        casa: 'Colômbia',
+        fora: 'RD Congo',
+        placar: '- : -',
+        local: 'Estádio Akron, Zapopan',
+        status: 'agendado'
+    },
+     {
+        data: '24/06/2026',
+        hora: '16:00',
+        casa: 'Suíça',
+        fora: 'Canadá',
+        placar: '- : -',
+        local: 'Hard Rock Stadium, Miami Gardens',
+        status: 'agendado'
+    },
+     {
+        data: '24/06/2026',
+        hora: '16:00',
+        casa: 'Bósnia e Herzegovina',
+        fora: 'Qatar',
+        placar: '- : -',
+        local: 'Lumen Field, Seattle',
+        status: 'agendado'
+    },
+     {
+        data: '24/06/2026',
+        hora: '19:00',
+        casa: 'Marrocos',
+        fora: 'Haiti',
+        placar: '- : -',
+        local: 'Mercedes-Benz Stadium, Atlanta',
+        status: 'agendado'
+    },
+     {
+        data: '24/06/2026',
+        hora: '19:00',
+        casa: 'Escócia',
+        fora: 'Brasil',
+        placar: '- : -',
+        local: 'BC Place, Vancouver',
+        status: 'agendado'
+    },
     
-    
-    
-    
-    
+     {
+        data: '24/06/2026',
+        hora: '22:00',
+        casa: 'África do Sul',
+        fora: 'Coréia do Sul',
+        placar: '- : -',
+        local: 'Estádio BBVA, Guadaloupe',
+        status: 'agendado'
+    },
+      {
+        data: '24/06/2026',
+        hora: '22:00',
+        casa: 'Republica Tcheca',
+        fora: 'México',
+        placar: '- : -',
+        local: 'Estádio Azteca, Cidade do México',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '17:00',
+        casa: 'Curaçao',
+        fora: 'Costa do Marfim',
+        placar: '- : -',
+        local: 'Lincoln Financial Field, Filadélfia',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '17:00',
+        casa: 'Equador',
+        fora: 'Alemanha',
+        placar: '- : -',
+        local: 'MetLife, East Rutheford',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '20:00',
+        casa: 'Tunísia',
+        fora: 'Holanda',
+        placar: '- : -',
+        local: 'Arrowhead Stadium, Kansas City',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '20:00',
+        casa: 'Japão',
+        fora: 'Suécia',
+        placar: '- : -',
+        local: 'AT&T Stadium, Arlington',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '23:00',
+        casa: 'Turquia',
+        fora: 'EUA',
+        placar: '- : -',
+        local: 'Sofi Stadium, Inglewood',
+        status: 'agendado'
+    },
+     {
+        data: '25/06/2026',
+        hora: '23:00',
+        casa: 'Paraguai',
+        fora: 'Austrália',
+        placar: '- : -',
+        local: 'Levis Stadium, Santa Clara',
+        status: 'agendado'
+    },
+     {
+        data: '26/06/2026',
+        hora: '16:00',
+        casa: 'Noruega',
+        fora: 'França',
+        placar: '- : -',
+        local: 'Gillete Stadium, Foxborough',
+        status: 'agendado'
+    },
+    {
+        data: '26/06/2026',
+        hora: '16:00',
+        casa: 'Senegal',
+        fora: 'Iraque',
+        placar: '- : -',
+        local: 'BMO Field, Toronto',
+        status: 'agendado'
+    },
+    {
+        data: '26/06/2026',
+        hora: '21:00',
+        casa: 'Cabo Verde',
+        fora: 'Arábia Saudita',
+        placar: '- : -',
+        local: 'NRG Stadium, Houston',
+        status: 'agendado'
+    },
+    {
+        data: '26/06/2026',
+        hora: '21:00',
+        casa: 'Uruguai',
+        fora: 'Espanha',
+        placar: '- : -',
+        local: 'Estádio Akron, Zapopan',
+        status: 'agendado'
+    },
+    {
+        data: '27/06/2026',
+        hora: '00:00',
+        casa: 'Nova Zelândia',
+        fora: 'Bélgica',
+        placar: '- : -',
+        local: 'BC Place, Vancouver',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '00:00',
+        casa: 'Egito',
+        fora: 'Irã',
+        placar: '- : -',
+        local: 'Lumen Field, Seattle',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '18:00',
+        casa: 'Panamá',
+        fora: 'Inglaterra',
+        placar: '- : -',
+        local: 'MetLife Stadium, East Rutherford',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '18:00',
+        casa: 'Croácia',
+        fora: 'Gana',
+        placar: '- : -',
+        local: 'Lincoln Financial Field, Filadélfia',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '20:30',
+        casa: 'Colômbia',
+        fora: 'Portugal',
+        placar: '- : -',
+        local:'Hard Rock Stadium, Miami Gardens',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '27:30',
+        casa: 'RD Congo',
+        fora: 'Uzbequistão',
+        placar: '- : -',
+        local: 'Mercedes-Benz-Stadium, Atlanta',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '23:00',
+        casa: 'Argélia',
+        fora: 'Áustria',
+        placar: '- : -',
+        local: 'Arrowhead Stadium, Kansas City',
+        status: 'agendado'
+    },
+     {
+        data: '27/06/2026',
+        hora: '23:00',
+        casa: 'Jordânia',
+        fora: 'Argentina',
+        placar: '- : -',
+        local: 'AT&T Stadium, Arlington',
+        status: 'agendado'
+    },
    
     
 ]
@@ -340,7 +769,7 @@ const coresSelecoes = {
     'Inglaterra': '#fff',
     'Croácia': '#FF0000',
     'Gana': '#006B3F',
-    'Panamá': '#072357'
+    'Panamá': '#1b53ba'
 
    
 }
